9ad297723bcf71b0f7a43dfdfcae77809b35dcb3
Reaxoh <64882473+Reaxoh@users.noreply.github.com>
2023-04-12 14:05:42 +0000

    feat: Add `mysql2` and link to SQL
    This commit adds mysql2 and link to the relevant SQL file.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
 const app = express();
+const mysql = require('mysql2');
+const databaseConfig  =  require('./config/databaseConfig.json')
 
 app.use(cors());
 app.use(express.json());
@@ -10,10 +12,18 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// DB Setting
+const connection = mysql.createConnection({
+    host: databaseConfig.host,
+    user: databaseConfig.user,
+    password: databaseConfig.password,
+    database: databaseConfig.database
+})
 
 // Router Setting
 const homeRoutes = require('./routes/homeRoutes');
 const usersRoutes = require('./routes/api/users');
+const { config } = require('process');
 app.use('/', homeRoutes);
 app.use('/api/users', usersRoutes);
 
@@ -28,8 +38,36 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
+connection.connect((err) => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+    connection.query('SHOW DATABASES', (error, results, fields) => {
+        if (error) throw error;
+    
+        console.log('Databases:');
+        console.log(results);
+    
+        // Retrieve table information
+        connection.query('SHOW TABLES', (error, results, fields) => {
+          if (error) throw error;
+    
+          console.log('Tables:');
+          console.log(results);
+    
+          // Close the connection
+          connection.end((err) => {
+            if (err) throw err;
+            console.log('Connection closed.');
+          });
+        });      
+    });
+})
+
 // Start Express service
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
