01b97821a9b4e7963fc66e14899f42e6b7a14acf
Hsun <hsun1031@outlook.com>
2023-05-01 04:43:47 +0000

    🎨 style: Make console prettier

--- routes/espdev.js
+++ routes/espdev.js
@@ -10,13 +10,15 @@ router.post('/get/status', async (req, res) => {
     const user  = req.get('user');
     const token = req.get('token');
     // const data  = req.body;
-    // console.log(`🚀 ~ file: espdev.js:23 ~ router.post ~ [${user}: ${token}]:`, data);
+    // const currentDate = new Date();
+    // console.log(`[L][${currentDate.toLocaleString()}]📝 espdev.js[/get/status] 🔊 [${user}: ${token}]data: ${data}.`);
 
     // Check token
     myDatabase.checkMachineToken(user, token)
         .then((results) => {
             if(results == false) {
-                console.error(`[POST] /api/espdev/get/status: [401: Unauthorized]${user}`);
+                const currentDate = new Date();
+                console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/get/status] 🔊 checkMachineToken error: Unauthorized.`);
                 res.status(401).send("Unauthorized");
                 return;
             } else {
@@ -24,22 +26,27 @@ router.post('/get/status', async (req, res) => {
                 myDatabase.getMachineState(user)
                     .then((results) => {
                         if(results >= 0) {
-                            // console.log(`[POST] /api/espdev/get/status: [${user}]status: ${results}`);
+                            // const currentDate = new Date();
+                            // console.log(`[L][${currentDate.toLocaleString()}]📝 espdev.js[/get/status] 🔊 getMachineState [${user}]status: ${results}.`);
                             res.status(200).send(`results${results}`);
                         } else {
+                            const currentDate = new Date();
+                            console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/get/status] 🔊 getMachineState error: results is out of range.`);
                             res.status(500).send("Server Error");
-                            return;;
+                            return;
                         }
                     })
                     .catch((err) => {
-                        console.log("🚀 ~ file: espdev.js:45 ~ router.post ~ err:", err);
+                        const currentDate = new Date();
+                        console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/get/status] 🔊 getMachineState error: ${err}.`);
                         res.status(500).send("Server Error");
                         return;
                     });
             }
         })
         .catch((err) => {
-            console.log("🚀 ~ file: espdev.js:52 ~ router.post ~ err:", err);
+            const currentDate = new Date();
+            console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/get/status] 🔊 checkMachineToken error: ${err}.`);
             res.status(500).send("Server Error");
             return;
         });
@@ -53,10 +60,13 @@ router.post('/set/status', async (req, res) => {
     const user  = req.get('user');
     const token = req.get('token');
     const data  = req.body;
-    // console.log(`🚀 ~ file: espdev.js:51 ~ router.post ~ [${user}: ${token}]:`, data);
+    // const currentDate = new Date();
+    // console.log(`[L][${currentDate.toLocaleString()}]📝 espdev.js[/set/status] 🔊 [${user}: ${token}]data: ${data}.`);
 
     try {
         if(data.status === undefined) {
+            const currentDate = new Date();
+            console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/set/status] 🔊 data status is undefined.`);
             res.status(400).send("Bad Request");
             return;
         } 
@@ -64,10 +74,14 @@ router.post('/set/status', async (req, res) => {
         status = parseInt(data.status);
 
         if(status < 0 || status > 6) {
+            const currentDate = new Date();
+            console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/set/status] 🔊 data status is out of range.`);
             res.status(400).send("Bad Request");
             return;
         }
     } catch (error) {
+        const currentDate = new Date();
+        console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/set/status] 🔊 data status is error: ${error}.`);
         res.status(400).send("Bad Request");
         return;
     }
@@ -76,35 +90,42 @@ router.post('/set/status', async (req, res) => {
     myDatabase.checkMachineToken(user, token)
         .then((results) => {
             if(results == false) {
-                console.error(`[POST] /api/espdev/set/status: [401: Unauthorized]${user}`);
+                const currentDate = new Date();
+                console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/set/status] 🔊 [${user}]checkMachineToken: results is false.`);
                 res.status(401).send("Unauthorized");
                 return;
             } else {
                 myDatabase.setMachineState(user, status)
                     .then((results) => {
                         if(results == true) {
-                            console.log(`[POST] /api/espdev/set/status: [${user}]status: ${results}`);
+                            // const currentDate = new Date();
+                            // console.log(`[L][${currentDate.toLocaleString()}]📝 espdev.js[/set/status] 🔊 [${user}]setMachineState: results${status}`);
                             res.status(200).send(`Success results${status}`);
                             return;
                         } else {
+                            const currentDate = new Date();
+                            console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/set/status] 🔊 [${user}]setMachineState: results is false.`);
                             res.status(500).send("Server Error");
                             return;;
                         }
                     })
                     .catch((err) => {
-                        console.log("🚀 ~ file: espdev.js:85 ~ router.post ~ err:", err);
+                        const currentDate = new Date();
+                        console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/set/status] 🔊 [${user}]setMachineState: error: ${err}.`);
                         res.status(500).send("Server Error");
                         return;
                     });
             }
         })
         .catch((err) => {
-            console.log("🚀 ~ file: espdev.js:92 ~ router.post ~ err:", err);
+            const currentDate = new Date();
+            console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/set/status] 🔊 [${user}]checkMachineToken: error: ${err}.`);
             res.status(500).send("Server Error");
             return;
         });
 });
 
+// [POST] /api/espdev/get/url
 router.post('/get/url', async (req, res) => {
     try {
         const user  = req.get('user');
@@ -112,54 +133,30 @@ router.post('/get/url', async (req, res) => {
         myDatabase.getOTAInfo(user, token)
             .then((results) => {
                 if(results.status != 1) {
-                    console.error(`[POST] /api/espdev/get/url: [401: Unauthorized]${user}`);
+                    const currentDate = new Date();
+                    console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/get/url] 🔊 [${user}]getOTAInfo: Unauthorized.`);
                     res.status(401).send("Unauthorized");
                     return;
                 } else {
+                    const currentDate = new Date();
+                    console.log(`[L][${currentDate.toLocaleString()}]📝 espdev.js[/get/url] 🔊 [${user}]getOTAInfo: url: ${results.url}`);
                     res.status(200).send(results.url);
                     return;
                 }
             })
             .catch((err) => {
-                console.log("🚀 ~ file: espdev.js:111 ~ router.post ~ err:", err);
+                const currentDate = new Date();
+                console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/get/url] 🔊 [${user}]getOTAInfo: error: ${err}.`);
                 res.status(500).send("Server Error");
                 return;
             }
         );
 
     } catch(err) {
-        console.log("espdev.js [POST]/get/url err:", err);
+        const currentDate = new Date();
+        console.error(`[E][${currentDate.toLocaleString()}]📝 espdev.js[/get/url] 🔊 [${user}]error: ${err}.`);
         res.status(400).send("Bad Request");
     }
 });
-
-/**
- * 
- * @param   { String } user   - MAC address of the machine.
- * @param   { String } token  - Token of the machine.
- * @returns { JSON   } my_res - Response data, code and message.
- */
-function checkToken(user, token) {
-    let my_res = {
-        code: 200,
-        message: {}
-    }
-
-    myDatabase.checkMachineToken(user, token)
-        .then((results) => {
-            if(results == false) {
-                console.log(`User: ${user} Token ${token} not found in database.`);
-                my_res.code = 401;
-                my_res.message = { status: "error", code: code, message: "Unauthorized" };
-            }
-        })
-        .catch((err) => {
-            console.log("🚀 ~ file: espdev.js:28 ~ router.post ~ err:", err)
-            my_res.code = 500;
-            my_res.message = { status: "error", code: code, message: "Server Error" };
-        });
-    
-    return my_res;
-}
 
 module.exports = router;
