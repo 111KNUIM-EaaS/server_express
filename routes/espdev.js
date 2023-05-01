2e317a1fdc461e116517fe30311e0b798d6ca9ad
Hsun <hsun1031@outlook.com>
2023-05-01 04:08:28 +0000

    👽️ feat: Change Database API

--- routes/espdev.js
+++ routes/espdev.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Database = require('../database/database.js').Database;
+const Database = require('../database/database.js').DatabaseESP;
 const myDatabase = new Database();
 
 // [POST] /api/espdev/get/status
