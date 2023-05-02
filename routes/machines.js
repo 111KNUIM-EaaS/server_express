214313046897bb8d5ec484e78a253686c6dcbcfd
Hsun <hsun1031@outlook.com>
2023-05-02 08:35:04 +0000

    🎨 style: Remove unused code

--- routes/machines.js
+++ routes/machines.js
@@ -77,46 +77,6 @@ router.post('/borrow/state', (req, res) => {
     }
 });
 
-router.post('/return/time', (req, res) => {
-    const returnTimeString = req.body.returnTime;
-    const returnTime =  moment(returnTimeString, "YYYY-MM-DD HH:mm:ss").utcOffset(8).format("YYYY-MM-DD HH:mm:ss");
-    const user_uid = req.body.uid;
-    const machine_id = req.body.machineID;
-
-    myDatabase.addReturn(user_uid, machine_id, returnTime).then((results) => {
-        data = { status: "success", data: results };
-        res.status(200).send(data);
-        myDatabase.getMachineTime(user_uid).then((results) => {
-            console.log("🚀 ~ file: machines.js:72 ~ myDatabase.addReturn ~ results:", results);
-            
-            let rental_time = results[0].rental_time;
-            let return_time = results[0].return_time;
-            let price       = results[0].price;
-            let rental_time_moment = moment(rental_time, "YYYY-MM-DD HH:mm:ss").utcOffset(8);
-            let return_time_moment = moment(return_time, "YYYY-MM-DD HH:mm:ss").utcOffset(8);
-            let time_diff = return_time_moment.diff(rental_time_moment, 'seconds');
-            let total_price = (price  * time_diff ).toFixed(2);
-            let duration = new Date(0, 0, 0, 0, 0, time_diff);
-            let timeString = moment(duration).format('HH:mm:ss');
-
-            console.log(`🚀 ~ file: machines.js:63 ~ myDatabase.addReturn ~ time_diff:" ${time_diff} seconds`);
-            console.log(`🚀 ~ file: machines.js:64 ~ myDatabase.addReturn ~ total_price:" ${total_price} dollars`);
-            console.log(`🚀 ~ file: machines.js:65 ~ myDatabase.addReturn ~ timeString:" ${timeString} `);
-            
-            myDatabase.addBill(user_uid, machine_id, timeString, total_price);
-            myDatabase.returnMachine(machine_id);
-            myDatabase.deleteMachineBorrow(machine_id);
-            
-        })
-    }).catch((err) => {
-        console.error(err);
-        res.status(500).send('Server Error');
-    });
-    console.log("🚀 ~ file: machines.js:52 ~ router.post ~ return_id:", machine_id);
-    console.log("🚀 ~ file: machines.js:50 ~ router.post ~ returnTime:", returnTime);
-    console.log("req.body:", req.body);
-});
-
 router.post('/state', (req, res) => {
     const uid = req.body.uid;
     console.log("🚀 ~ file: machines.js:55 ~ router.post ~ uid:", uid);
